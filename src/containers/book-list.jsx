bfffa56a880d0c9e7efbc6ffa9367a58f91bca20
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-18 14:48:46 -0700

    updated app and index

--- src/containers/book-list.jsx
+++ src/containers/book-list.jsx
@@ -18,3 +18,11 @@ class BookList extends Component {
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'Kitchen Confidential'},
-    {title: 'Elquent Ruby'},
-    {title: 'The dark Tower'}
-  ]
+    { title: 'Javascript: The Good Parts'},
+    { title: 'Harry Potter'},
+    { title: 'Kitchen Confidential'},
+    { title: 'Elquent Ruby'},
+    { title: 'The dark Tower'}
+  ];
 }
