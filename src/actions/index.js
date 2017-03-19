35a43adb4f47268f05427753a5a7ecfdf892dd28
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-19 16:41:02 -0700

    added axios  url request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
 
 const API_KEY = '208678fd1ba9778a0868e3d862e5d808';
+const ROOOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
